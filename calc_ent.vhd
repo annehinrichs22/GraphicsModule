dd8b934f461445ed2de9e831cdcbe6a13014966f
Anne Hinrichs <annehinrichs22@gmail.com>
2022-01-13 15:53:08 +0100

    Update calc_ent.vhd

--- calc_ent.vhd
+++ calc_ent.vhd
@@ -4,7 +4,7 @@ use IEEE.std_logic_1164.ALL;
 entity calculation is
     port(screen_x     : in  std_logic_vector(8 downto 0); -- 0 -> 511
          screen_y     : in  std_logic_vector(7 downto 0); -- 0 -> 255, van 128 tot 256 is de track zichtbaar
-         player_angle : in  std_logic_vector(8 downto 0);
+         orientations : in  std_logic_vector(8 downto 0);
          player_x     : in  std_logic_vector(9 downto 0);
          player_y     : in  std_logic_vector(9 downto 0);
          sample_x     : out std_logic_vector(9 downto 0);
